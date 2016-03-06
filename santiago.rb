29c88a192d2b3004d1c7edf3dbf8696229335913
Santiago <smarcosh@gmail.com>
2016-03-06 10:47:27 -0800

    Santiago's file added

--- santiago.rb
+++ santiago.rb
@@ -1,1 +1,10 @@
-#Santiago write a program that only accepts integers that are evenly divisible by 5. That is to say there will be no remainder.
+#Santiago write a program that only accepts integers that are evenly divisible by 5. 
+#That is to say there will be no remainder.
+puts 'Enter an integer that is evenly divisible by 5:'
+num = gets.chomp.to_i
+if num % 5 == 0
+	puts "Great, thank you"
+else
+	puts 'try again'
+end
+
